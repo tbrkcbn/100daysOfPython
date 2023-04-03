eff7df0f13791e724c5352e517e19c7994db0d32
BIM3235 <tugberkcoban@anadoluhayat.com.tr>
2023-04-03 19:16:23 +0300

    main loop is added

---
+++ main.py
@@ -0,0 +1,10 @@
+# variable definitions:
+choice = ""
+
+# class definitions
+
+
+# main loop cycle
+
+while not choice == "T":
+    choice = input("“What would you like? (espresso/latte/cappuccino):")
