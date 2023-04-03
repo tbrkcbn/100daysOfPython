0c602051c0ebcd04ea13b3c441765ae8e9d07ac8
BIM3235 <tugberkcoban@anadoluhayat.com.tr>
2023-04-03 19:29:53 +0300

    report function added

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 # variable definitions:
 choice = ""
-
+water = 1000
+coffeeGrains = 1000
+milk = 1000
+money = 0
 # class definitions
 
+# function definitions
+
+def report():
+    print(f"Water:   {water}ml")
+    print(f"Coffee:  {coffeeGrains}ml")
+    print(f"Milk:    {milk}ml")
+    print(f"Money:   {money}$")
 
 # main loop cycle
 
-while not choice == "T":
-    choice = input("“What would you like? (espresso/latte/cappuccino):")
+while not choice == "off":
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice  == "espresso":
+        print
+    elif choice  == "report":
+        report()
+    elif choice  == "latte":
+        print
+    elif choice  == "cappuccino":
+        print
+    else :
+        print("Choose again")
+
+print("Adios amigo!")
