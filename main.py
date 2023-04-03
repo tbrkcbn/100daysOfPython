109e7d91160864a1e5c48e400e05a39aba29621f
BIM3235 <tugberkcoban@anadoluhayat.com.tr>
2023-04-04 00:47:38 +0300

    all job has done

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-File_object = open(r"kullanicilar.txt","r")
+users_file = open(r"kullanicilar.txt","r")
+
+Lines = users_file.readlines()
+
+
+
+count = 0
+
+for line in Lines:
+    count += 1
+    # print("Line{}: {}".format(count, line.strip()))
+
+print(f"Tekilleştirmeden önceki kullanıcı sayısı: {count}")
+
+count = 0
+
+Lines = list(dict.fromkeys(Lines))
+
+for line in Lines:
+    count += 1
+    # print("Line{}: {}".format(count, line.strip()))
+
+print(f"Tekilleştirdikten sonraki kullanıcı sayısı: {count}")
+
+users_file.close()
+Lines.sort()
+fp = open("tekilleştirilmiş_kullanıcılar.txt", "w")
+
+for line in Lines:
+    fp.write("%s" % line)
+print("Done")
+
+fp.close()
